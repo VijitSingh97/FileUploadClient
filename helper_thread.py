349f387ef72e7710bfccad53e115e6e4801e752b
Vijit Singh <vijit.singh@aa.com>
2022-02-23 14:00:02 -0600

    initial

---
+++ helper_thread.py
@@ -0,0 +1,12 @@
+
+
+
+class HelperThread():
+    
+    # run every 5 secs
+    def sync():
+        #
+        # check files in /data and put time stamp in list
+        # GET /status -> return list of files and time stamps
+        # compare on client
+        # sync old files with /download and upload new files with /upload
---
+++ main.py
@@ -0,0 +1,7 @@
+from FileServerClient import FileServerClient
+# import helper thread
+
+# start helper thread
+#   helper will sync ...
+
+FileServerClient().cmdloop()
